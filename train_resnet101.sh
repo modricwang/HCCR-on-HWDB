5b40307157d53fff676c35c1cceeebdd43f76852
Modric Wang <modricwang@126.com>
2018-02-22 05:45:35 +0800

    script bug fix

--- train_resnet101.sh
+++ train_resnet101.sh
@@ -5,14 +5,13 @@ mkdir checkpoints
 python -u main.py \
     -shuffle \
     -train_record \
-    -model resnet152 \
+    -model resnet101 \
     -data_dir ./data \
     -save_path checkpoints \
-    -color_classes 10 \
-    -type_classes 7 \
+    -output_classes 3755 \
     -n_epochs 150 \
     -learn_rate 0.003 \
-    -pretrained ./pretrain/resnet152-b121ed2d.pth \
+    -pretrained ./pretrain/resnet101-5d3b4d8f.pth \
     -batch_size 64 \
     -workers 32 \
     -nGPU 1 \
