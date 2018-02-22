09457c4fb68cd1ce5a86045ebee1a7fa8d6b0a9f
Modric Wang <modricwang@126.com>
2018-02-22 20:39:45 +0800

    update params

--- train_resnet101.sh
+++ train_resnet101.sh
@@ -10,9 +10,9 @@ python -u main.py \
     -save_path checkpoints \
     -output_classes 3755 \
     -n_epochs 150 \
-    -learn_rate 0.003 \
+    -learn_rate 0.01 \
     -pretrained ./pretrain/resnet101-5d3b4d8f.pth \
     -batch_size 8 \
-    -workers 2 \
+    -workers 0 \
     -nGPU 1 \
 2>&1 | tee train.log
